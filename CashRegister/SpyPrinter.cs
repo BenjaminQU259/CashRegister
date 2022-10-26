402e817915f732a13d5e94174cbf381a31e7840b
Minghao QU <MQu2@slb.com>
2022-10-26 14:49:17 +0800

    feat: reformat SpyPrinter

--- CashRegister/SpyPrinter.cs
+++ CashRegister/SpyPrinter.cs
@@ -5,9 +5,9 @@ namespace CashRegister
 		public bool HasPrinted { get; set; }
 		public override void Print(string content)
 		{
-			base.Print(content);
-			HasPrinted = true;
       // send message to a real printer
+      base.Print(content);
+      HasPrinted = true;
     }
 	}
 }
